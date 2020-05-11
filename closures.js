ee4a6ac3cdbb08d054f9fc2422621528912360a8
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-11 16:27:49 -0600

    Solution for Closures

--- closures.js
+++ closures.js
@@ -22,7 +22,7 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+inner = outer()
 
 
 
@@ -51,7 +51,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend('Jake')
 
 
 
@@ -61,16 +61,21 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  let num = 1
+  return function(){
+    return num++
+  }
+} 
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+   var count = makeCounter();
+   count(); // 1
+   count(); // 2
+   count(); // 3
+   count(); // 4
 
 
 
@@ -86,18 +91,25 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
+  
   return {
+    inc: function(){
+      value = value + 1
+      return value
+    },
+    dec: function(){
+      value = value - 1
+      return value
+    }
 
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -111,12 +123,13 @@ counter = counterFactory(10);
 
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
+  
+  return function message(){
+    welcome = `${welcomeText} ${firstname} ${lastname}.`
+  return welcome;
+  }
+};
 
-  // code message function here.
-
-  //Uncommment this to return the value of your message function
-  //return message;
-}
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
 
@@ -143,7 +156,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod(){
+      return privateMethod()
+    }
   };
 })();
 
@@ -162,7 +177,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(num){
+      secret += num
+      return secret
+    },
+    takeAwayFromSecret: function(num){
+      secret -= num
+      return secret
+    }
   };
 }
 
@@ -188,8 +210,9 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    let time = i
     setTimeout(function() {
-      console.log(i);
+      console.log(time);
     }, i * 1000);
   }
 }
